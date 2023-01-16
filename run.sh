50a2ee6a015b9fce29bccd74d9be413b926724c7
seiya8 <seiya.111122@gmail.com>
2023-01-16 19:17:33 +0900

    readでtokenとかを取得

--- run.sh
+++ run.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
+read -p "token: " TOKEN
+read -p "json file: " JSON_FILE
+read -p "folder id: " FOLDER_ID
+if [ ! -e $JSON_FILE ]; then
+    echo "JSON file does not exist" 1>&2
+    exit 1
+fi
 echo 'exporting slack data'
 echo '----'
-python export_slack_data.py $1
+python export_slack_data.py $TOKEN
 echo '----'
 echo 'uploading data to spreadsheet'
 echo '----'
-python upload_to_spreadsheet.py $2 $3
+python upload_to_spreadsheet.py $JSON_FILE $FOLDER_ID
 echo 'done!'
