c36dc00e08c8dd460ec8acbf68808389975046ab
seiya8 <seiya.111122@gmail.com>
2023-01-16 18:32:10 +0900

    csvダンプまで1つにまとめた

--- export_slack_data.py
+++ export_slack_data.py
@@ -1,4 +1,6 @@
 import codecs
+import csv
+from datetime import datetime
 import json
 import os
 import requests
@@ -67,20 +69,44 @@ def get_thread_list(headers, params):
 def download_files(channel_name, thread_list):
     for thread in thread_list:
         for message_dict in thread:
-            if 'files' in message_dict:
-                for file_dict in message_dict['files']:
-                    dl_url = file_dict['url_private_download']
-                    content = fetch_file(dl_url, headers)
+            if 'files' not in message_dict: continue
+            for file_dict in message_dict['files']:
+                dl_url = file_dict['url_private_download']
+                content = fetch_file(dl_url, headers)
 
-                    save_dirname = os.path.join('output', 'attached_files', channel_name, message_dict['ts'])
-                    os.makedirs(save_dirname, exist_ok=True)
-                    save_file(content, os.path.join(save_dirname, file_dict['name']))
+                save_dirname = os.path.join('output', 'attached_files', channel_name, message_dict['ts'])
+                os.makedirs(save_dirname, exist_ok=True)
+                save_file(content, os.path.join(save_dirname, file_dict['name']))
+
+def create_csv(channel_name, user_dict, thread_list):
+    with open(os.path.join('output', 'csv_files', f'{channel_name}.csv'), 'w') as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"', lineterminator='\n')
+        for thread in thread_list:
+                for i, message in enumerate(thread):
+                    post_time = datetime.fromtimestamp(int(float(message['ts']))).strftime('%Y-%m-%d %H:%M')
+                    name = user_dict[message['user']]
+                    post = '' if i else message['text']
+                    reply = message['text'] if i else ''
+
+                    for user_id, user_name in user_dict.items():
+                        post = post.replace(user_id, user_name)
+                        reply = reply.replace(user_id, user_name)
+
+                    file_name = ''
+                    if 'files' in message:
+                        dirname = os.path.join('output', 'attached_files', channel_name, message['ts'])
+                        for i, file in enumerate(message['files']):
+                            file_name = os.path.join(dirname, file['name'])
+                            if i: post, reply = '', ''
+                            writer.writerow([post_time, name, post, file_name, reply])
+                    else:
+                        writer.writerow([post_time, name, post, file_name, reply])
 
 if __name__ == '__main__':
     os.makedirs('output/json_files', exist_ok=True)
+    os.makedirs('output/csv_files', exist_ok=True)
 
-    token = sys.argv[1]
-    headers = {'Authorization': f'Bearer {token}'}
+    headers = {'Authorization': f'Bearer {sys.argv[1]}'}
 
     user_dict = parse_user_json(headers)
     dump_json(user_dict, 'users.json')
@@ -88,12 +114,13 @@ if __name__ == '__main__':
     dump_json(channel_dict, 'channels.json')
     
     for channel_id, channel_name in channel_dict.items():
-        print(channel_name)
+        print(f'channel: {channel_name}')
 
         params = {'channel': channel_id, 'limit': '1000', 'oldest': '0'}
         thread_list = get_thread_list(headers, params)
 
         os.makedirs(f'output/attached_files/{channel_name}', exist_ok=True)
-        save_message_path = os.path.join('output/json_files', f'{channel_name}_message.json')
+        save_message_path = os.path.join('output/json_files', f'{channel_name}.json')
         dump_json(thread_list, save_message_path)
+        create_csv(channel_name, user_dict, thread_list)
         download_files(channel_name, thread_list)
