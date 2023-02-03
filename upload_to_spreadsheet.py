bda4ec33c52e26ddce54557f486b17b29bd5cb14
seiya8 <seiya.sasa8@gmail.com>
2023-02-03 22:24:48 +0900

    fetch profile images

--- upload_to_spreadsheet.py
+++ upload_to_spreadsheet.py
@@ -20,12 +20,12 @@ class DriveUploader:
             'title': file_name,
             'parents': [{'id': parents_id}],
             'mimeType': mimeType
-        } 
+        }
         time.sleep(1)
         folder = self.drive.CreateFile(file_metadata)
         folder.Upload()
         return folder['id']
-        
+
     def upload_file(self, parents_id, file_path):
         file_metadata = {
             'title': file_path.split('/')[-1],
@@ -55,10 +55,10 @@ if __name__ == '__main__':
     for channel_name in channel_dict.values():
         print(f'channel: {channel_name}')
         ws = sh.add_worksheet(title=channel_name, rows=1000, cols=26)
-            
+
         set_column_widths(ws, [('A', 120), ('B', 120), ('C', 600), ('D', 300), ('E', 600)])
         ws.format('A:E', {'wrapStrategy': 'WRAP', 'verticalAlignment': 'TOP'})
-            
+
         time.sleep(1)
         channel_folder_id = uploader.create_file(folder_id, channel_name, 'application/vnd.google-apps.folder')
         with open(f'output/csv_files/{channel_name}.csv') as f:
@@ -67,12 +67,12 @@ if __name__ == '__main__':
                 if row[3]:
                     ts = row[3].split('/')[-2]
                     subfolder_id = uploader.create_file(channel_folder_id, ts, 'application/vnd.google-apps.folder')
-                    
+
                     time.sleep(1)
                     file_id = uploader.upload_file(subfolder_id, row[3])
                     file_name = row[3].split('/')[-1]
                     row[3] = f'=HYPERLINK("https://drive.google.com/file/d/{file_id}", "{file_name}")'
-                    
+
                 cell_list = ws.range(i+1, 1, i+1, 5)
                 for j, v in enumerate(row):
                     cell_list[j].value = v
